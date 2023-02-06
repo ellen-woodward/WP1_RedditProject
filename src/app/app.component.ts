820ca8828226ccab1a75466af3d35e21fcc60bf7
ellen-woodward <s00222321@mail.itsligo.ie>
2023-02-06 20:46:12 +0000

    created addArticle method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'reddit-project';
+  addArticle(title:HTMLInputElement, link:HTMLInputElement):Boolean{
+    console.log(`Adding Article ${title.value} : Adding Link ${link.value}`);
+    return false;
+  }
 }
